5b326fea6e1cfe8fe573d28b6d35f3615a7df117
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-03-09 20:26:13 +0530

    Update index.js
    /book/:name == /books/:name

--- A3_Middlewares/index.js
+++ A3_Middlewares/index.js
@@ -6,7 +6,7 @@ app.get("/books", allBooks, (req, res) => {
     return res.send({ rotue: "books" });
 });
 
-app.get("/book/:name", singleBook, (req, res) => {
+app.get("/books/:name", singleBook, (req, res) => {
     return res.send({ bookName: req.name });
 });
 
