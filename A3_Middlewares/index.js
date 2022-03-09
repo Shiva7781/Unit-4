c0b77f59f768da0540fcfe2ab6d14222f5abda4b
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-03-09 17:53:16 +0530

    A3

---
+++ A3_Middlewares/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/books", allBooks, (req, res) => {
+    return res.send({ rotue: "books" });
+});
+
+app.get("/book/:name", singleBook, (req, res) => {
+    return res.send({ bookName: req.name });
+});
+
+function allBooks(req, res, next) {
+    console.log("Fetching all books");
+
+    next();
+}
+
+function singleBook(req, res, next) {
+    req.name = req.params.name;
+
+    next();
+}
+
+app.listen(5000, () => {
+    console.log("listening on port 5000");
+});
