f90a395c7b4699e37b2b5aa9bf66d031e195d068
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-03-13 10:48:25 +0530

    Added console.log( "Fetching a Book")

--- A3_Middlewares/index.js
+++ A3_Middlewares/index.js
@@ -6,7 +6,7 @@ app.get("/books", allBooks, (req, res) => {
     return res.send({ rotue: "books" });
 });
 
-app.get("/books/:name", singleBook, (req, res) => {
+app.get("/book/:name", singleBook, (req, res) => {
     return res.send({ bookName: req.name });
 });
 
@@ -17,7 +17,9 @@ function allBooks(req, res, next) {
 }
 
 function singleBook(req, res, next) {
+    
     req.name = req.params.name;
+    console.log("Fetching a book");
 
     next();
 }
@@ -25,3 +27,6 @@ function singleBook(req, res, next) {
 app.listen(5000, () => {
     console.log("listening on port 5000");
 });
+
+
+
