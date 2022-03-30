b20c2a1d1b66598ff77031a40fdcf7f47b0b7dab
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-03-30 16:44:44 +0530

    A12

---
+++ A12_Authorisation_Oauth/A12_Autho_Oauth/src/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const connect = require("./configs/db");
+const passport = require("./configs/google-auth");
+const userController = require("./controllers/user.controller")
+const productController = require("./controllers/product.controller")
+
+const {register,login,generateToken} = require("./controllers/auth.controller")
+const app = express();
+
+app.use(express.json());
+
+
+app.use("/users", userController)
+
+app.post("/register", register)
+
+app.post("/login", login)
+
+app.use("/products", productController)
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+  	[ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+	passport.authenticate( 'google', {
+		successRedirect: '/auth/google/success',
+		failureRedirect: '/auth/google/failure'
+}));
+
+app.listen(5000, async () => {
+    try{
+        await connect();
+        console.log("listening on port 5000")
+    }
+    catch(err){
+        console.log(err.message);
+    }
+});
--- A12_Authorisation_Oauth/src/configs/google-oauth.js
+++ A12_Authorisation_Oauth/src/configs/google-oauth.js
@@ -1,36 +1,43 @@
-var GoogleStrategy = require(`passport-google-oauth2`).Strategy
-const passport = require(`passport`)
-const { v4: uuidv4 } = require(`uuid`)
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+const passport = require('passport');
+const { v4: uuidv4 } = require('uuid');
+const User = require("../models/user.model");
+require("dotenv").config();
 
-const User = require("../models/user.model")
-
-require(`dotenv`).config()
 
 passport.use(new GoogleStrategy({
 
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4000/auth/google/callback",
-    passReqToCallback: true
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:4000/auth/google/callback",
+
+  // callbackURL: "http://localhost:4000/auth/google/callback
+  // what is this for
+  passReqToCallback: true
 },
-    async (request, accessToken, refreshToken, profile, done) => {
+  async function (request, accessToken, refreshToken, profile, done) {
+    let user = await User.findOne({ email: profile?._json?.email });
 
-        let user = await User.findOne({ email: profile._json.email }).lean().exec()
+    if (!user) {
+      user = await User.create({
+        email: profile._json.email,
+        password: uuidv4(),
+        category: ["seller"]
+      })
+    }
 
-        if (!user) {
+    return done(err, user);
 
-            user = await User.create({
+    // console.log({request: request});
+    // console.log({accessToken: accessToken});
+    // console.log({refreshToken: refreshToken});
+    console.log({ email: profile._json.email });
+    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    //   return done(err, user);
+    // });
 
-                email: profile._json.email,
-                password: uuidv4(),
-                category: [`seller`]
-            })
-        }
+    // for dummy password uuidv4();
+  })
+);
 
-        return done(err, user)
-
-        // console.log({email: profile._json.email})
-    })
-)
-
-module.exports = passport
+module.exports = passport;
